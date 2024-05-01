c093d4f1c886390dbe9be06f8bc8de715b2d4a69
Noah Burghardt <burghardtnoah@gmail.com>
2024-05-01 03:01:38 -0600

    broke up docker deploy pipeline

--- generate.py
+++ generate.py
@@ -1,102 +1,20 @@
-import yaml
-from jinja2 import Environment, FileSystemLoader
-from jinja2_extensions import setup_filters
-from helpers import docker_build, python_build, docker_deploy
-import os
-
-def load_file(fn):
-    with open(fn, 'r') as f:
-        return f.read()
-
-def load_yaml(fn):
-    with open(fn, 'r') as f:
-        return yaml.safe_load(f)
-
-_jinja_env = None
-def get_jinja():
-    global _jinja_env
-    if _jinja_env is None:
-        env = Environment(loader=FileSystemLoader('templates'))
-        setup_filters(env)
-        _jinja_env = env
-    return _jinja_env
-
-_env: dict[str, str] | None = None
-def get_env():
-    global _env
-    if _env is None:
-        env = {}
-        def cpy(key):
-            nonlocal env
-            env[key] = os.environ[key]
-        def trycpy(key):
-            nonlocal env
-            if key in os.environ:
-                env[key] = os.environ[key]
-
-        cpy('GITLAB_CR_REGISTRY')
-        cpy('DOCKER_HUB_REPOSITORY')
-
-        trycpy('CI_COMMIT_TAG')
-        trycpy('CI_COMMIT_BRANCH')
-        cpy('CI_COMMIT_SHORT_SHA')
-        cpy('CI_PIPELINE_SOURCE')
-
-        _env = env
-
-    return _env
-
-def get_public_env():
-    private_env = get_env()
-    env = {}
-    for key in [
-        'GITLAB_CR_REGISTRY',
-        'DOCKER_HUB_REPOSITORY',
-        'CI_COMMIT_TAG',
-        'CI_COMMIT_BRANCH',
-        'CI_COMMIT_SHORT_SHA',
-        'CI_PIPELINE_SOURCE'
-        ]:
-        if key in private_env:
-            env[key] = private_env[key]
-    return env
+from render_template import load_pipeline_config, get_env, get_public_env, render_template, deduplicate_keys
 
 def generate_steps(data):
     tasks = data.get('tasks', [])
-
     env = get_env()
-    output = []
-    for task in tasks:
-        task_type = task.get('type')
-
-        if task_type == 'docker-build':
-            template = get_jinja().get_template('docker-build.yml.jinja')
-            rendered = template.render(helpers=docker_build, task=task, env=env)
-            output.append(rendered)
-        if task_type == 'python-build':
-            template = get_jinja().get_template('python-build.yml.jinja')
-            rendered = template.render(helpers=python_build, task=task, env=env)
-            output.append(rendered)
-        if task_type == 'docker-deploy':
-            template = get_jinja().get_template('docker-deploy.yml.jinja')
-            rendered = template.render(helpers=docker_deploy, task=task, env=env)
-            output.append(rendered)
-
+    output = [render_template(env, task) for task in tasks]
     return '\n'.join(output)
 
-def deduplicate_keys(yaml_data: str):
-    data = yaml.safe_load(yaml_data)
-    return yaml.dump(data)
 
 def main():
+    data = load_pipeline_config()
+    steps = generate_steps(data)
+    dd_steps = deduplicate_keys(steps)
+
     public_env = get_public_env()
     for k, v in public_env.items():
         print(f'# {k}: {v}')
-
-    pipeline_file = 'pipeline.yml'
-    data = load_yaml(pipeline_file)
-    steps = generate_steps(data)
-    dd_steps = deduplicate_keys(steps)
 
     print(dd_steps)
 
---
+++ render_template.py
@@ -0,0 +1,136 @@
+import yaml, argparse
+from jinja2 import Environment, FileSystemLoader
+from jinja2_extensions import setup_filters
+from helpers import docker_build, python_build, docker_deploy
+import os
+import functools
+
+@functools.cache
+def get_images():
+    return {
+        'docker': 'docker:26.0.2',
+        'hephaestus': 'registry.gitlab.com/haondt/cicd/registry/hephaestus:1.0.0',
+        'docker_deployer': 'registry.gitlab.com/haondt/cicd/registry/docker-deployer:latest',
+        'python': 'registry.gitlab.com/haondt/cicd/registry/python-builder:1.0.2'
+    }
+
+def load_file(fn):
+    with open(fn, 'r') as f:
+        return f.read()
+
+def load_pipeline_config():
+    pipeline_file = 'pipeline.yml'
+    data = load_yaml(pipeline_file)
+    if 'tasks' not in data:
+        data['tasks'] = []
+    for i in range(len(data['tasks'])):
+        data['tasks'][i].index = i
+    return data
+
+def load_yaml(fn):
+    with open(fn, 'r') as f:
+        return yaml.safe_load(f)
+
+_jinja_env = None
+def get_jinja():
+    global _jinja_env
+    if _jinja_env is None:
+        env = Environment(loader=FileSystemLoader('templates'))
+        setup_filters(env)
+        _jinja_env = env
+    return _jinja_env
+
+_env: dict[str, str] | None = None
+def get_env():
+    global _env
+    if _env is None:
+        env = {}
+        def cpy(key):
+            nonlocal env
+            env[key] = os.environ[key]
+        def trycpy(key):
+            nonlocal env
+            if key in os.environ:
+                env[key] = os.environ[key]
+
+        cpy('GITLAB_CR_REGISTRY')
+        cpy('DOCKER_HUB_REPOSITORY')
+
+        trycpy('CI_COMMIT_TAG')
+        trycpy('CI_COMMIT_BRANCH')
+        cpy('CI_COMMIT_SHORT_SHA')
+        cpy('CI_PIPELINE_SOURCE')
+
+        _env = env
+
+    return _env
+
+def get_public_env():
+    private_env = get_env()
+    env = {}
+    for key in [
+        'GITLAB_CR_REGISTRY',
+        'DOCKER_HUB_REPOSITORY',
+        'CI_COMMIT_TAG',
+        'CI_COMMIT_BRANCH',
+        'CI_COMMIT_SHORT_SHA',
+        'CI_PIPELINE_SOURCE'
+        ]:
+        if key in private_env:
+            env[key] = private_env[key]
+    return env
+
+def render_template(env, task, task_type = None):
+    task_type = task_type or task.get('type')
+
+    if task_type == 'docker-build':
+        template = get_jinja().get_template('docker-build.yml.jinja')
+        return template.render(helpers=docker_build, task=task, env=env, images=get_images())
+    if task_type == 'python-build':
+        template = get_jinja().get_template('python-build.yml.jinja')
+        return template.render(helpers=python_build, task=task, env=env, images=get_images())
+    if task_type == 'docker-deploy':
+        template = get_jinja().get_template('docker-deploy.yml.jinja')
+        return template.render(helpers=docker_deploy, task=task, env=env, images=get_images())
+    if task_type == 'noop':
+        template = get_jinja().get_template('noop.yml.jinja')
+        return template.render()
+    if task_type == 'docker-deploy-downstream':
+        template = get_jinja().get_template('docker-deploy-downstream.yml.jinja')
+        return template.render(helpers=docker_deploy, task=task, env=env, images=get_images())
+    raise ValueError(f"unknown task type: {task_type}")
+
+def deduplicate_keys(yaml_data: str):
+    data = yaml.safe_load(yaml_data)
+    return yaml.dump(data)
+
+def main():
+    parser = argparse.ArgumentParser(prog='render-build')
+    parser.add_argument('task_index', help='index of task to render for')
+    parser.add_argument('template', help='template to render', choices=[
+        'docker-build',
+        'python-build',
+        'docker-deploy',
+        'docker-deploy-downstream',
+        'noop'
+        ])
+    args = parser.parse_args()
+    template_name = args.template
+    task_i = args.task_index
+
+    data = load_pipeline_config()
+    task = data['tasks'][int(task_i)]
+
+    env = get_env()
+    public_env = get_public_env()
+    for k, v in public_env.items():
+        print(f'# {k}: {v}')
+
+
+    rendered = render_template(env, task, template_name)
+    dd_rendered = deduplicate_keys(rendered)
+
+    print(dd_rendered)
+
+if __name__ == '__main__':
+    main()
