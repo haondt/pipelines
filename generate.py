f0ad863dcf5886eafbaf25ae7894dfdc710fb08c
Noah Burghardt <burghardtnoah@gmail.com>
2024-05-17 15:03:21 -0600

    fixed template joining

--- generate.py
+++ generate.py
@@ -1,22 +1,25 @@
 from render_template import load_pipeline_config, get_env, get_public_env, render_template, deduplicate_keys
+from utils import deep_merge
+import yaml
 
 def generate_steps(data):
     tasks = data.get('tasks', [])
     env = get_env()
-    output = [render_template(env, task) for task in tasks]
-    return '\n'.join(output)
+    rendered_templates = [render_template(env, task) for task in tasks]
+    merged_output = {}
+    for rendered in rendered_templates:
+        merged_output = deep_merge(merged_output, yaml.safe_load(rendered))
 
+    return yaml.dump(merged_output)
 
 def main():
     data = load_pipeline_config()
     steps = generate_steps(data)
-    dd_steps = deduplicate_keys(steps)
-
     public_env = get_public_env()
     for k, v in public_env.items():
         print(f'# {k}: {v}')
 
-    print(dd_steps)
+    print(steps)
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -15,3 +15,36 @@ def try_get_version(tag: str):
     if not regex_ismatch(tag, r'^v\d+\.\d+\.\d+$'):
         return None
     return tag[1:]
+
+# deep merge two dictionaries created from yaml
+# when merging primitives, the one from d2 is preferred
+def deep_merge(d1, d2, conflicts="new", path=""):
+    if conflicts not in ["new", "old", "err"]:
+        raise ValueError("Unexpected conflict resolution:" + conflicts)
+    def merge_list(l1, l2):
+        return list(set(l1 + l2))
+    result = d1.copy()
+    for k, v in d2.items():
+        if k not in result:
+            result[k] = v
+            continue
+        if type(v) != type(result[k]):
+            if conflicts == "new":
+                result[k] = v
+            elif conflicts == "err":
+                raise KeyError(f"Multiple entries found for key: {path}.{k}")
+            continue
+        if isinstance(v, dict):
+            result[k] = deep_merge(result[k], v, conflicts, path + "." + k)
+            continue
+        if isinstance(v, (tuple, list)):
+            result[k] = merge_list(result[k], v)
+            continue
+
+        if conflicts == "new":
+            result[k] = v
+        elif conflicts == "err":
+            raise KeyError(f"Multiple entries found for key: {path}.{k}")
+    return result
+
+
