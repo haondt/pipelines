0e8f71e7d6888e3e0bd61046260d57f5fa1b85f9
Noah Burghardt <burghardtnoah@gmail.com>
2024-09-23 05:54:50 -0600

    updated docker deployer image

--- render_template.py
+++ render_template.py
@@ -10,7 +10,7 @@ def get_images():
     return {
         'docker': 'docker:26.0.2',
         'hephaestus': 'registry.gitlab.com/haondt/cicd/registry/hephaestus:1.0.1',
-        'docker_deployer': 'registry.gitlab.com/haondt/cicd/registry/docker-deployer:1.1.5',
+        'docker_deployer': 'registry.gitlab.com/haondt/cicd/registry/docker-deployer:1.1.8',
         'python': 'registry.gitlab.com/haondt/cicd/registry/python-builder:2.0.0',
         'docs': 'registry.gitlab.com/haondt/cicd/registry/docs-builder:1.0.1',
         'dotnet': 'registry.gitlab.com/haondt/cicd/registry/dotnet-builder:0.0.1',
