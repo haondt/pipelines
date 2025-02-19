69145cb5c5cfb5767700dd1ec3adbd2202138a89
Noah Burghardt <burghardtnoah@gmail.com>
2025-02-19 09:51:12 -0700

    updated errmsg

--- helpers/python_build.py
+++ helpers/python_build.py
@@ -3,7 +3,7 @@ from utils import try_get_version
 def get_version(env):
     commit_tag = env.get('CI_COMMIT_TAG')
     if commit_tag is None:
-        raise RuntimeError('not a commit pipeline')
+        raise RuntimeError('no commit tag associated with pipeline')
     version = try_get_version(commit_tag)
     if version is None:
         raise ValueError(f'commit tag `{commit_tag}` not in expected format')
